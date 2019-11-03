88870fd6a29be2dc35dcf4a318365493e7c30afa
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2019-11-03 10:26:40 +0100

    -

--- Formula.v
+++ Formula.v
@@ -2255,6 +2255,45 @@ induction l as [| a l]; intros. {
   exfalso.
   remember (is_prime n) as b eqn:Hb; symmetry in Hb.
   destruct b; [ now rewrite prime_decomp_of_prime in Hl | ].
+(**)
+  unfold is_prime in Hb.
+  unfold prime_decomp in Hl.
+  replace n with (S (S (n - 2))) in Hb at 1 by flia Hn.
+  replace n with (S (S (n - 2))) in Hl at 1 by flia Hn.
+  destruct n; [ easy | ].
+  destruct n; [ easy | clear Hn ].
+  replace (S (S n) - 2) with n in Hb by flia.
+  destruct n; [ easy | ].
+  remember (S (S n)) as sn.
+clear Hb.
+(*
+  cbn - [ "/" "mod" ] in Hb.
+*)
+  cbn - [ "/" "mod" ] in Hl.
+  remember (S sn mod 2) as b1 eqn:Hb1; symmetry in Hb1.
+  destruct b1; [ now apply prime_decomp_aux_succ_pow_not_nil in Hl | ].
+(*
+  Hl : prime_decomp_aux sn (S sn) 3 0 = []
+*)
+  subst sn.
+  remember (S n) as sn.
+(*
+  cbn - [ "/" "mod" ] in Hb.
+*)
+  cbn - [ "/" "mod" ] in Hl.
+  remember (S (S sn) mod 3) as b2 eqn:Hb2; symmetry in Hb2.
+  destruct b2; [ now apply prime_decomp_aux_succ_pow_not_nil in Hl | ].
+(*
+  Hl : prime_decomp_aux sn (S (S sn)) 4 0 = []
+*)
+  subst sn.
+  cbn - [ "/" "mod" ] in Hl.
+  remember (S (S (S n)) mod 4) as b3 eqn:Hb3; symmetry in Hb3.
+  move b3 before b2.
+  destruct b3; [ now apply prime_decomp_aux_succ_pow_not_nil in Hl | ].
+(*
+  Hl : prime_decomp_aux n (S (S (S n))) 5 0 = []
+*)
 ...
 
 (* https://en.wikipedia.org/wiki/Factorial#Number_theory *)
