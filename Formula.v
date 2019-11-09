30e8198fcee9dcfa4266e31fba25e7be5464e8c2
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2019-11-09 19:26:17 +0100

    -

--- Formula.v
+++ Formula.v
@@ -2448,7 +2448,22 @@ destruct niter; cbn; rewrite Hb. {
     etransitivity; [ apply phony_prime_after_is_after | ].
     remember (fact (n + 1) + 1) as it1 eqn:Hit1.
     replace (fact n + 1) with (S (fact n)) by flia; cbn.
-    rewrite Hb; subst it1.
+    rewrite Hb.
+...
+    remember (fact n) as it2 eqn:Hit2.
+...
+    clear - Hb1.
+    revert n it2 Hb1.
+    induction it1; intros. {
+      cbn.
+      destruct it2; [ easy | cbn ].
+      destruct (is_prime (n + 1)); [ easy | apply Nat.le_0_l ].
+    }
+    cbn; rewrite Hb1.
+    destruct it2; [ now cbn; rewrite Hb1 | ].
+    cbn.
+    admit.
+  }
 ...
   }
 ...
