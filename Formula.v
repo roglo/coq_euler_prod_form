91e17396340b0bcaf9f3a7dd12a3a7f6ba1728b3
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2020-01-02 19:14:12 +0100

    -

--- Formula.v
+++ Formula.v
@@ -3437,7 +3437,14 @@ cbn.
 remember (negb (a mod p =? 0)) as b eqn:Hb; symmetry in Hb.
 destruct b. {
   rewrite not_div_cons_r.
+  rewrite app_length; symmetry.
+  rewrite not_div_cons_r; symmetry.
   rewrite app_length.
+  rewrite IHl.
+  remember (length (not_div pl [a])) as len eqn:Hlen; symmetry in Hlen.
+  destruct len; [ easy | ].
+  destruct len. {
+Compute (let (pl, a) := ([2; 3; 4; 5], 30) in (not_div pl [a])).
 ...
 
 Theorem partial_φ_cons : ∀ m p pl,
