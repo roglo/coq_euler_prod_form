6e13d9ea58a246af2f600cb905e139121f9c155b
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2020-01-31 17:39:56 +0100

    -

--- Formula.v
+++ Formula.v
@@ -3527,6 +3527,7 @@ induction k. {
   destruct (lt_dec mn 2) as [H2n| H2n]. {
     destruct mn as [| mn]. {
       cbn.
+(* fail *)
 (* perhaps n must be prime and, in that case, we are
    in a field? *)
 ...
