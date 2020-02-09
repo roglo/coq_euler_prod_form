e03a24f4277516f92aa2e28fc4944aefdf6e8fa1
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2020-02-09 17:30:39 +0100

    -

--- Rpolynomial.v
+++ Rpolynomial.v
@@ -12,20 +12,32 @@ Class ring α :=
   { rng_zero : α;
     rng_one : α;
     rng_add : α → α → α;
+    rng_mul : α → α → α;
     rng_opp : α → α }.
 
 Declare Scope ring_scope.
 Delimit Scope ring_scope with Rng.
+Notation "0" := rng_zero : ring_scope.
 Notation "1" := rng_one : ring_scope.
 Notation "a + b" := (rng_add a b) : ring_scope.
+Notation "a * b" := (rng_mul a b) : ring_scope.
 Notation "- a" := (rng_opp a) : ring_scope.
+
+Notation "'Σ' ( i = b , e ) , g" :=
+  (fold_left (λ c i, (c + g)%Rng) (seq b (S e - b)) 0%Rng)
+  (at level 45, i at level 0, b at level 60, e at level 60) : ring_scope.
 
 (* lap : list as polynomial, i.e. the only field of the record in the
    definition of polynomial after *)
 
-Definition lap_1 {A} {rng : ring A} := [1%Rng].
+Section Lap.
 
-Fixpoint lap_add {A} {rng : ring A} al₁ al₂ :=
+Variable A : Type.
+Variable rng : ring A.
+
+Definition lap_1 := [1%Rng].
+
+Fixpoint lap_add al₁ al₂ :=
   match al₁ with
   | [] => al₂
   | a₁ :: bl₁ =>
@@ -35,8 +47,15 @@ Fixpoint lap_add {A} {rng : ring A} al₁ al₂ :=
       end
   end.
 
-Definition lap_opp {A} {rng : ring A} l := map (λ a, (- a)%Rng) l.
-Definition lap_sub {A} {rng : ring A} la lb := lap_add la (lap_opp lb).
+Definition lap_opp l := map (λ a, (- a)%Rng) l.
+Definition lap_sub la lb := lap_add la (lap_opp lb).
+
+Definition lap_convol_mul_term la lb i :=
+  (Σ (j = 0, i), List.nth j la 0 * List.nth (i - j)%nat lb 0)%Rng.
+Definition polm_mul la lb :=
+  map (lap_convol_mul_term la lb) (seq 0 (length la + length lb - 1)).
+
+Definition xpow i := repeat 0%Rng i ++ [1%Rng].
 
 (*
 ... to be continued from "Formula.v"
