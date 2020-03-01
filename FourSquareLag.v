253602a7a1edd9ad78b89cf9a622e0b46616f40a
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2020-03-02 00:59:44 +0100

    -

--- FourSquareLag.v
+++ FourSquareLag.v
@@ -700,4 +700,13 @@ exfalso; apply H1; [ | | | easy ]. {
 }
 Qed.
 
-Inspect 1.
+Definition diff x y := if lt_dec x y then y - x else x - y.
+
+Theorem Euler_s_four_square_identity : ∀ a1 a2 a3 a4 b1 b2 b3 b4,
+  (a1 ^ 2 + a2 ^ 2 + a3 ^ 2 + a4 ^2) * (b1 ^ 2 + b2 ^ 2 + b3 ^ 2 + b4 ^ 2) =
+     diff (a1 * b1) (a2 * b2 + a3 * b3 + a4 * b4) ^ 2 +
+     diff (a1 * b2 + a2 * b1 + a3 * b4) (a4 * b3) ^ 2 +
+     diff (a1 * b3 + a3 * b1 + a4 * b2) (a2 * b4) ^ 2 +
+     diff (a1 * b4 + a2 * b3 + a4 * b1) (a3 * b2) ^ 2.
+Proof.
+...
