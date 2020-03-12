33f2a69bae127201f3fc59a1efcf70650929bd64
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2020-03-12 01:30:10 +0100

    -

--- FourSquareEuler.v
+++ FourSquareEuler.v
@@ -1,15 +1,12 @@
 (* Euler's four square identity *)
 
-Require Import Utf8 ZArith Psatz.
-Require Import Misc.
-
-Definition Z_diff x y := (x - y)%Z.
+Require Import Utf8 ZArith.
 
 Definition Z_Euler_s_four_square_sol a b c d p q r s :=
   (a * p + b * q + c * r + d * s,
-   Z_diff (a * q + d * r) (b * p + c * s),
-   Z_diff (a * r + b * s) (c * p + d * q),
-   Z_diff (a * s + c * q) (b * r + d * p))%Z.
+   a * q + d * r - (b * p + c * s),
+   a * r + b * s - (c * p + d * q),
+   a * s + c * q - (b * r + d * p))%Z.
 
 Theorem Z_Euler_s_four_square_identity : ∀ a b c d p q r s,
   let '(x, y, z, t) := Z_Euler_s_four_square_sol a b c d p q r s in
@@ -18,6 +15,5 @@ Theorem Z_Euler_s_four_square_identity : ∀ a b c d p q r s,
 Proof.
 intros.
 unfold Z_Euler_s_four_square_sol.
-unfold Z_diff.
 ring.
 Qed.
