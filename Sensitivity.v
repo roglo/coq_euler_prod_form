a4fe84c08e6883052f5018d14c86f160ee3b8d33
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2020-03-18 09:31:56 +0100

    -

--- Sensitivity.v
+++ Sensitivity.v
@@ -27,13 +27,7 @@ Compute (let n := 3 in map (λ a, (a, filter (are_adjacent_vertices a) (seq 0 (2
 
 (* subgraph of the n-dimensional cube graph *)
 
-Definition subgraph_prop n vl := ∀ a, a ∈ vl → a < 2 ^ n.
-
-Record subgraph n := mksg
-  { sg_vert : list nat;
-    sg_prop : subgraph_prop n sg_vert }.
-
-Arguments sg_vert {n}.
+Record subgraph := mksg { sg_vert : list nat }.
 
 Definition edges vl :=
   fold_right
@@ -47,25 +41,17 @@ Definition edges vl :=
 
 Compute (edges [1; 2; 7; 4]).
 
-Definition sg_edges {n} (sg : subgraph n) := edges (sg_vert sg).
+Definition sg_edges sg := edges (sg_vert sg).
 
 (* Example *)
 
 Definition cube_vert := seq 0 (2 ^ 3).
-Definition cube_prop : subgraph_prop 3 cube_vert.
-Proof.
-intros a Ha; cbn.
-do 8 (destruct Ha as [H| Ha]; [ flia H | ]).
-easy.
-Qed.
-Definition cube_edges := edges cube_vert.
-
-Definition full_cube := mksg 3 cube_vert cube_prop.
+Definition full_cube := mksg cube_vert.
 
 (* edges and vertices count *)
 
-Definition number_of_edges {n} (sg : subgraph n) := length (sg_edges sg).
-Definition number_of_vertices {n} (sg : subgraph n) := length (sg_vert sg).
+Definition number_of_edges sg := length (sg_edges sg).
+Definition number_of_vertices sg := length (sg_vert sg).
 Compute (number_of_edges full_cube).
 Compute (number_of_vertices full_cube).
 
@@ -75,14 +61,15 @@ Definition vdeg el v :=
   count_occ Bool.bool_dec (map (λ '(a, b), Nat.eqb a v) el) true +
   count_occ Bool.bool_dec (map (λ '(a, b), Nat.eqb v b) el) true.
 
-Definition deg {n} (sg : subgraph n) v := vdeg (sg_edges sg) v.
+Definition deg sg v := vdeg (sg_edges sg) v.
 
 (* Δ : maximum degree of a subgraph *)
 
-Definition vΔ n vl :=
-  fold_left (λ m v, max m (vdeg vl v)) (seq 0 (2 ^ n - 1)) 0.
+Definition vΔ vl :=
+  let el := edges vl in
+  fold_left (λ m v, max m (vdeg el v)) vl 0.
 
-Definition Δ {n} (sg : subgraph n) := vΔ n (sg_edges sg).
+Definition Δ sg := vΔ (sg_vert sg).
 
 (* testing... *)
 
@@ -90,29 +77,36 @@ Compute (Δ full_cube, Nat.sqrt 3).
 Compute (2 ^ (3 - 1) + 1).
 
 Compute (length (sg_edges full_cube)).
-Compute (vdeg cube_edges 0).
-Compute (map (λ i, nth i cube_edges (0, 0)) [0; 5; 8]).
-Compute (vdeg (map (λ i, nth i cube_edges (0, 0)) [0; 5; 8]) 0).
-Compute (vdeg (map (λ i, nth i cube_edges (0, 0)) [0; 5; 8]) 1).
-Compute (vdeg (map (λ i, nth i cube_edges (0, 0)) [0; 5; 8]) 2).
-Compute (vΔ 3 (map (λ i, nth i cube_edges (0, 0)) [0; 5; 8])).
-Compute (Nat.sqrt 3).
+Compute (vdeg (edges cube_vert) 0).
 
 Compute (edges [1; 2; 4; 7]).
 Compute (length (edges [1; 2; 4; 7])).
 Compute (2 ^ (3 - 1) + 1).
 
-Compute (vΔ 3 (edges [0; 1; 4; 5; 7])).
+Compute (vΔ [0; 1; 4; 5; 7]).
 Compute (edges [0; 1; 4; 5; 7]).
 
 Compute (edges [0; 3; 5; 6; 9; 10; 12; 15]).
-Compute (vΔ 4 (edges [0; 3; 5; 6; 9; 10; 12; 15])).
+Compute (vΔ [0; 3; 5; 6; 9; 10; 12; 15]).
 Compute (2 ^ (4 - 1) + 1).
 Compute (Nat.sqrt 4).
-Compute (map (λ i, vΔ 4 (edges [0; 3; 5; 6; 9; 10; 12; 15; i])) (seq 0 16)).
-Compute (map (λ i, vΔ 3 (edges [0; 3; 5; 6; i])) (seq 0 8)).
+Compute (let n := 4 in 2 ^ (n - 1) + 1).
+Compute (map (λ i, vΔ [0; 3; 5; 6; 9; 10; 12; 15; i]) (seq 0 16)).
+Compute (let n := 4 in Nat.sqrt n).
+Compute (let n := 3 in 2 ^ (n - 1) + 1).
+Compute (map (λ i, (i, vΔ [0; 3; 5; 6; i])) (seq 0 8)).
+Compute (let n := 3 in Nat.sqrt n).
 
-...
+Compute (edges [0; 3; 5; 6]).
+Compute (edges [0; 3; 5; 6; 2]).
+Compute (vdeg (edges [0; 3; 5; 6; 1])) 1.
+
+Compute (Nat.sqrt 5).
+Compute (let n := 5 in 2 ^ (n - 1) + 1).
+Compute (length [0; 3; 5; 6; 9; 10; 12; 15; 17; 18; 20; 23; 24; 27; 29; 30]).
+Compute (edges [0; 3; 5; 6; 9; 10; 12; 15; 17; 18; 20; 23; 24; 27; 29; 30]).
+Compute (map (λ i, vΔ [0; 3; 5; 6; 9; 10; 12; 15; 17; 18; 20; 23; 24; 27; 29; 30; i]) (seq 0 32)).
+
 
 (* The theorem *)
 
