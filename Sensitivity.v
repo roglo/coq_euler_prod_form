b3d2fef55c3d706c3bf1b8829244be08e8687bd7
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2020-03-26 19:58:07 +0100

    -

--- Sensitivity.v
+++ Sensitivity.v
@@ -261,6 +261,8 @@ Fixpoint to_radix_loop it n i :=
 Definition to_radix n i := to_radix_loop i n i.
 Compute (map (to_radix 3) (seq 0 (3 ^ 3))).
 Compute (map (dispatch 3) (map (to_radix 3) (seq 0 (3 ^ 3)))).
+Print dispatch.
+Print disp_loop.
 (* mouais non, c'est pas bon, "dispatch" n'est pas adapté à "to_radix"
    car il demande que l'entrée soit une liste de listes de longueur n,
    ce n'est pas ce que renvoie to_radix *)
