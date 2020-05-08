2d44d2c8dab926175eebecfad48b375210818740
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2020-05-08 12:11:39 +0200

    -

--- Sensitivity.v
+++ Sensitivity.v
@@ -2189,6 +2189,10 @@ intros.
 Compute (let n:= 3 in to_radix n (fold_left (λ a i, a * n + i) (seq 0 n) 0) = rev (seq 0 n)).
 Print to_radix.
 Print to_radix_loop.
+assert (fold_left (λ a i, a * n + i) (seq 0 n) 0 mod n = n - 1). {
+  induction n; [ easy | ].
+  cbn - [ "mod" ].
+  (* c'est nul, pas assez général, mon colonel *)
 ...
 Compute (let l := [2; 2; 0] in let n := length l in let it := 7 in firstn n (to_radix_loop it n (fold_left (λ a j, a * n + j) l 0))).
 assert
